eb44bc49782dd478f13ee632732237a362732b18
kirill-k88 <kirill-k88@ya.ru>
2023-02-09 15:54:02 +0300

    feature: header and profile

---
+++ pages/script.js
@@ -0,0 +1,8 @@
+(600 / 9) *
+  (TypeConvert.ToUint1(unit.ACS_Security.EnableFormsHistory.Value) +
+    TypeConvert.ToUint1(unit.ACS_Security.EnableAlarms.Value) +
+    TypeConvert.ToUint1(unit.ACS_Security.EnableTrends.Value) +
+    TypeConvert.ToUint1(unit.ACS_Security.EnablePrint.Value) +
+    TypeConvert.ToUint1(unit.ACS_Security.EnableScale.Value) +
+    TypeConvert.ToUint1(unit.ACS_Security.EnablePresets.Value) +
+    TypeConvert.ToUint1(unit.ACS_Security.EnableForwardBackward.Value));
