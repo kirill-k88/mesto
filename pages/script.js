b025180699d083b54d5be08f8c361ac6a2158d1c
kirill-k88 <kirill-k88@ya.ru>
2023-02-10 11:08:29 +0300

    feature: create popup

--- pages/script.js
+++ pages/script.js
@@ -1,8 +1,24 @@
-(600 / 9) *
-  (TypeConvert.ToUint1(unit.ACS_Security.EnableFormsHistory.Value) +
-    TypeConvert.ToUint1(unit.ACS_Security.EnableAlarms.Value) +
-    TypeConvert.ToUint1(unit.ACS_Security.EnableTrends.Value) +
-    TypeConvert.ToUint1(unit.ACS_Security.EnablePrint.Value) +
-    TypeConvert.ToUint1(unit.ACS_Security.EnableScale.Value) +
-    TypeConvert.ToUint1(unit.ACS_Security.EnablePresets.Value) +
-    TypeConvert.ToUint1(unit.ACS_Security.EnableForwardBackward.Value));
+//Получить кнопки лайков всех карточек
+let buttonLikes = document.querySelectorAll(".cards__button-like");
+
+//Изменить svg-картинку лайка на карточке при нажатии
+buttonLikes.forEach((buttonLike) => {
+  buttonLike.addEventListener("click", function () {
+    let likeSvg = buttonLike.querySelector(".cards__button-like-svg");
+    likeSvg.classList.toggle("cards__button-like-svg_active");
+  });
+});
+
+//Получить кнопки показать и скрыть попап
+let buttonEdit = document.querySelector(".profile__button-edit");
+let buttonClose = document.querySelector(".popup__button-close");
+
+//Добавить событие нажатия
+buttonEdit.addEventListener("click", togglePopupVisability);
+buttonClose.addEventListener("click", togglePopupVisability);
+
+//Переключить состояние видимости попапа
+function togglePopupVisability() {
+  let popup = document.querySelector(".popup");
+  popup.classList.toggle("popup_opened");
+}
