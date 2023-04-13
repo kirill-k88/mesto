34f70f83c9022fc38f850972658b40d0dcdeb436
kirill-k88 <kirill-k88@ya.ru>
2023-04-13 11:41:10 +0300

    hotfix: modified targets for babel

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,22 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'entry',
-        corejs: 3,
-        targets: {
-          chrome: 90,
-          ie: 10,
-        },
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      // какой пресет использовать
+      targets: {
+        // какие версии браузеров поддерживать
+        edge: '17',
+        ie: '11',
+        firefox: '50',
+        chrome: '64',
+        safari: '11.1',
       },
-    ],
+
+      // использовать полифиллы для браузеров из свойства target
+      // по умолчанию babel использует поллифиллы библиотеки core-js
+      useBuiltIns: 'entry',
+    },
   ],
-};
+];
+
+module.exports = { presets };
